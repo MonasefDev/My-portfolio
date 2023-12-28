0fc161e0e8611ce108275400a763f64581c4d5e6
Monasef <monasef77@gmail.com>
2023-12-28 11:55:38 +0100

    initial setup project

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,97 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+:root {
+  /* color primary */
+  --color-primary-1: #01080E;
+  --color-primary-2: #011221;
+  --color-primary-3: #011627;
+  /* color secondary */
+  --color-grey-1: #607B96;
+  --color-teal-1: #3C9D93;
+  --color-purple-1 : #4D5BCE;
+  /* color accent */
+  --color-accent-1: #C98BDF;
+  --color-accent-2: #E99287;
+  --color-accent-3: #43D9AD;
+  --color-accent-4: #FEA55F;
+  /* color lines */
+  --color-lines : #1E2D3D;
+  --color-white : #FFFFFF;
+  /*color button */
+  --color-button-d-1: #FEA55F;
+  --color-button-d-2: #FFAC6B;
+  --color-button-p-1: #1C2B3A;
+  --color-button-p-2: #263B50;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  font-size: 62.5%;
+  font-weight: 400;
+  background-color: var(--color-primary-1);
+    height: 100%;
+  width: 100%;
+  padding: 30px;
+}
+
+body {
+  font-family: 'Fira Code', monospace;
+  color: var(--color-grey-1);
+  height: 100%;
+  width: 100%;
+  background-color: var(--color-primary-3);
+  font-size: 1.6rem;
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+  color: inherit;
+}
+
+button {
+  cursor: pointer;
+}
+
+*:disabled {
+  cursor: not-allowed;
+}
+
+
+/* Parent selector, finally 😃 */
+button:has(svg) {
+  line-height: 0;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+  hyphens: auto;
+}
+`;
+
+export default GlobalStyles;
