9fcd878e8bf9859be3fab6cc21281c090f47b35b
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-01-02 16:40:17 +0100

    Styled the heading, subheading, paragraphs, and links for a more structured and visually appealing display of project details

--- src/features/projects/ModelDetails.jsx
+++ src/features/projects/ModelDetails.jsx
@@ -1,29 +1,69 @@
 import React from "react";
+import styled from "styled-components";
+
+const DetailsContainer = styled.div`
+  margin-top: 2rem;
+  padding: 1.6rem;
+  border: 1px solid #ccc;
+  border-radius: .8rem;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+
+
+  .details-heading {
+    margin-bottom: 1.6rem;
+  }
+
+  .details-subheading {
+    margin-bottom: .8rem;
+  }
+
+  .details-paragraph {
+    margin-bottom: 1.2rem;
+  }
+
+  .details-link {
+    color: #3498db;
+    text-decoration: none;
+    margin-right: 1rem;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+`;
 
 function ModelDetails({ selectedProject }) {
   return (
-    <div>
-      <h2>Model Details</h2>
+    <DetailsContainer>
+      <h2 className="details-heading">Model Details</h2>
       {selectedProject ? (
         <div>
-          <h3>{selectedProject.title}</h3>
-          <p>Description: {selectedProject.description}</p>
-          <p>Details: {selectedProject.details}</p>
-          <p>Technologies: {selectedProject.technologies.join(", ")}</p>
-          <p>Features: {selectedProject.features.join(", ")}</p>
-          {/* Add more details as needed */}
-          <a href={selectedProject.github_link} target="_blank" rel="noopener noreferrer">
+          <h3 className="details-subheading">{selectedProject.title}</h3>
+          <p className="details-paragraph">Description: {selectedProject.description}</p>
+          <p className="details-paragraph">Details: {selectedProject.details}</p>
+          <p className="details-paragraph">Technologies: {selectedProject.technologies.join(", ")}</p>
+          <p className="details-paragraph">Features: {selectedProject.features.join(", ")}</p>
+          <a
+            className="details-link"
+            href={selectedProject.github_link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             View on GitHub
           </a>
-          <br />
-          <a href={selectedProject.live_link} target="_blank" rel="noopener noreferrer">
+          <a
+            className="details-link"
+            href={selectedProject.live_link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Live Demo
           </a>
         </div>
       ) : (
-        <p>Select a project to view details.</p>
+        <p className="details-paragraph">Select a project to view details.</p>
       )}
-    </div>
+    </DetailsContainer>
   );
 }
 
