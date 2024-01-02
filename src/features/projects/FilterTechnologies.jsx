2d081073177ba310e38a62376d5367d28ce4afe4
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-01-02 13:10:18 +0100

    Replaced the dropdown menu with checkboxes for filtering technologies

--- src/features/projects/FilterTechnologies.jsx
+++ src/features/projects/FilterTechnologies.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 
 function FilterTechnologies({ technologies, onFilterChange }) {
-  const handleFilterChange = (event) => {
+  const handleCheckboxChange = (event) => {
     const selectedTechnology = event.target.value;
     onFilterChange(selectedTechnology);
   };
 
   return (
     <div>
-      <label htmlFor="technologyFilter">Filter by Technology:</label>
-      <select id="technologyFilter" onChange={handleFilterChange}>
-        <option value="">All</option>
-        {technologies.map((technology) => (
-          <option key={technology} value={technology}>
-            {technology}
-          </option>
-        ))}
-      </select>
+      <h4>Filter by Technology:</h4>
+      {technologies.map((technology) => (
+        <div key={technology}>
+          <input
+            type="checkbox"
+            id={technology}
+            value={technology}
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor={technology}>{technology}</label>
+        </div>
+      ))}
     </div>
   );
 }
