9ec7814c5de5c15024e168c09d5f041c3acb91ca
monasefdev <monasef77@gmail.com>
2024-01-11 14:19:43 +0100

    add codesnippet in contact page using react-highlight

--- src/data/snippet.js
+++ src/data/snippet.js
@@ -1,10 +1,12 @@
 export const snippet1 = `
-    const button = document.querySelector('#sendBtn');
+const button = document.querySelector('#sendBtn');
 
 const message = {
 	name: "Jonathan Davis",
 	email: "jonathan-davis@gmail.com",
-	message: "Hey! Just checked your website and it looks awesome! Also, I checked your articled on Medium. Lerned a few nice tips. Thanks!",
+	message: "Hey! Just checked your website and it looks awesome!
+		 Also, I checked your articled on Medium.
+		Lerned a few nice tips. Thanks!",
 	date: "Thu 21 Apr"
 }
 
---
+++ src/features/contact/CodeSnippet.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Highlight from "react-highlight";
+import styled from "styled-components";
+import { snippet1 } from "../../data/snippet";
+
+function CodeSnippet() {
+  return (
+    <StyledCodeSnippet>
+      <Container>
+        <Highlight>{snippet1}</Highlight>
+      </Container>
+      <Scrollbar>
+        <div />
+      </Scrollbar>
+    </StyledCodeSnippet>
+  );
+}
+
+const StyledCodeSnippet = styled.div`
+  display: grid;
+  grid-template-columns: calc(100% - var(--scroll-width)) var(--scroll-width);
+  height: 100%;
+
+  border-left: ${(props) =>
+    props.mobile ? "none" : "1px solid var(--color-lines)"};
+  @media only screen and (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 2rem;
+  pre {
+    overflow: hidden;
+  }
+  code {
+    background-color: transparent !important;
+    width: 100%;
+    overflow-x: scroll;
+  }
+`;
+
+const Scrollbar = styled.div`
+  height: 100%;
+  border-left: 1px solid var(--color-lines);
+  padding: 4px;
+  & > div {
+    width: 100%;
+    height: 7px;
+    background-color: var(--color-grey-1);
+  }
+  @media only screen and (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+export default CodeSnippet;
