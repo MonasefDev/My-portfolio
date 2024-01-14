36b0461dc76f38e7bc08cba6cd57c2e649a58d79
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-01-14 18:26:44 +0100

    Update Projects page: improve styling, layout, and functionality

--- src/features/projects/FilterTechnologies.jsx
+++ src/features/projects/FilterTechnologies.jsx
@@ -3,24 +3,79 @@ import styled from "styled-components";
 
 const FilterContainer = styled.div`
   margin-bottom: 20px;
+  min-width: var(--sidebar-width);
+  border-right: 1px solid var(--color-lines);
+  height: 100vh;
+  padding: 0;
+`;
 
-  .filter-heading {
-    margin-bottom: 12px;
+const FilterHeading = styled.h4`
+  margin-bottom: 12px;
+  color: var(--color-white);
+  border-bottom: 1px solid var(--color-lines);
+  width: 100%;
+  padding: 1rem 1rem 1rem 2rem;
+`;
+
+const FilterItem = styled.div`
+  margin-bottom: 8px;
+  display: flex;
+  align-items: center;
+  padding: 0 0 0 1rem;
+
+  input:checked + label {
+    color: var(--color-white);
+  }
+`;
+
+const FilterInput = styled.input`
+  position: relative;
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  width: 1.8rem;
+  height: 1.8rem;
+  border: 2px solid var(--color-grey-1);
+  border-radius: 4px;
+  outline: none;
+  cursor: pointer;
+  margin-right: 2rem;
+
+  &:hover {
+    box-shadow: 0 0 5px rgba(96, 123, 150, 0.8);
   }
 
-  .filter-item {
-    margin-bottom: 8px;
-    display: flex;
-    align-items: center;
-
-    .filter-input {
-      margin-right: 8px;
-    }
-
-    .filter-label {
-      font-weight: 500;
-    }
+  &:checked {
+    background-color: var(--color-grey-1);
+    border-color: var(--color-grey-1);
   }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 1rem;
+    height: 0.5rem;
+    transition: opacity 0.3s ease;
+  }
+
+  &:checked::before {
+    content: "";
+    background-image: url("assets/icons/check.svg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 100%;
+    color: white;
+    width: 1.1rem;
+    height: 1.1rem;
+    opacity: 1;
+  }
+`;
+
+const FilterLabel = styled.label`
+  font-weight: 500;
 `;
 
 function FilterTechnologies({ technologies, onFilterChange }) {
@@ -31,20 +86,22 @@ function FilterTechnologies({ technologies, onFilterChange }) {
 
   return (
     <FilterContainer>
-      <h4 className="filter-heading">Filter by Technology:</h4>
+      <FilterHeading>
+        <img src="/assets/icons/arrow.svg" alt="arrow" /> projects
+      </FilterHeading>
       {technologies.map((technology) => (
-        <div key={technology} className="filter-item">
-          <input
+        <FilterItem key={technology}>
+          <FilterInput
             type="checkbox"
             id={technology}
             value={technology}
             onChange={handleCheckboxChange}
             className="filter-input"
           />
-          <label htmlFor={technology} className="filter-label">
+          <FilterLabel htmlFor={technology} className="filter-label">
             {technology}
-          </label>
-        </div>
+          </FilterLabel>
+        </FilterItem>
       ))}
     </FilterContainer>
   );
--- src/features/projects/ProjectCard.jsx
+++ src/features/projects/ProjectCard.jsx
@@ -2,14 +2,43 @@ import React from "react";
 import styled from "styled-components";
 
 const CardContainer = styled.div`
-  border: 1px solid #ccc;
+  max-width: 40rem;
+  min-height: 40rem;
   padding: 1.6rem;
+  .card-heading {
+    margin-bottom: 1rem;
+    color: var(--color-grey-1);
+    display: block;
+    font-weight: 500;
+
+    span {
+      color: var(--color-purple-2);
+      font-weight: 700;
+    }
+  }
+`;
+
+const CardDetails = styled.div`
+  border: 1px solid var(--color-lines);
   margin-bottom: 1.6rem;
-  border-radius: .8rem;
+  border-radius: 0.8rem;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 
-  .card-heading {
-    margin-bottom: .8rem;
+  .card-details {
+    padding: 3rem;
+  }
+
+  .card-image {
+    margin-bottom: 1.6rem;
+    height: 15rem;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 0.8rem 0.8rem 0 0;
+    }
   }
 
   .card-paragraph {
@@ -19,9 +48,9 @@ const CardContainer = styled.div`
   .card-button {
     background-color: var(--color-button-p-1);
     color: #fff;
-    padding: .8rem 1.6rem;
+    padding: 0.8rem 1.6rem;
     border: none;
-    border-radius: .4rem;
+    border-radius: 0.4rem;
     cursor: pointer;
   }
 
@@ -30,7 +59,7 @@ const CardContainer = styled.div`
   }
 `;
 
-function ProjectCard({ project, onSelectProject, onToggleModal }) {
+function ProjectCard({ project, index, onSelectProject, onToggleModal }) {
   const handleProjectSelect = () => {
     onSelectProject(project);
     onToggleModal(true);
@@ -38,11 +67,22 @@ function ProjectCard({ project, onSelectProject, onToggleModal }) {
 
   return (
     <CardContainer>
-      <h3 className="card-heading">{project.title}</h3>
-      <p className="card-paragraph">{project.description}</p>
-      <button className="card-button" onClick={handleProjectSelect}>
-        View Details
-      </button>
+      <h3 className="card-heading">
+        <span>Project {index + 1}</span>
+        {" // _"}
+        {project.title.split(" ").join("-")}
+      </h3>
+      <CardDetails>
+        <div className="card-image">
+          <img src={`${project.poster_img}${index + 1}`} alt={project.title} />
+        </div>
+        <div className="card-details">
+          <p className="card-paragraph">{project.description}</p>
+          <button className="card-button" onClick={handleProjectSelect}>
+            View Details
+          </button>
+        </div>
+      </CardDetails>
     </CardContainer>
   );
 }
