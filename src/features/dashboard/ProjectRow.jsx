fdbe65625251d958b50e189a655dbfe8ffbb7462
monasefdev <monasef77@gmail.com>
2024-01-23 17:24:43 +0100

    add table to projects list

---
+++ src/features/dashboard/ProjectRow.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Table from "../../ui/Table";
+
+function ProjectRow() {
+  return (
+    <Table.Row>
+      <div>project 1</div>
+      <div>github link</div>
+      <div>demo link</div>
+      <div>technologies</div>
+      <div></div>
+    </Table.Row>
+  );
+}
+
+export default ProjectRow;
--- src/features/dashboard/ProjectsList.jsx
+++ src/features/dashboard/ProjectsList.jsx
@@ -1,7 +1,39 @@
 import React from "react";
-
+import { projects } from "../../data/projects";
+import Table from "../../ui/Table";
+import styled from "styled-components";
+import ProjectRow from "./ProjectRow";
 function ProjectsList() {
-  return <div>list project</div>;
+  console.log(projects);
+  return (
+    <StyledProjectsList>
+      <Table columns="2fr 1fr 1fr 2fr 3.2rem">
+        <Table.Header>
+          <div>projects</div>
+          <div>github_link</div>
+          <div>demo_link</div>
+          <div>technologies</div>
+          <div></div>
+        </Table.Header>
+        <Table.Body>
+          <ProjectRow />
+        </Table.Body>
+        {/* <Table.Body
+          data={bookings}
+          render={(booking) => (
+            <BookingRow key={booking.id} booking={booking} />
+            )}
+          /> */}
+      </Table>
+    </StyledProjectsList>
+  );
 }
+
+const StyledProjectsList = styled.div`
+  padding: 4rem;
+  @media only screen and (max-width: 1024px) {
+    padding: 2rem;
+  }
+`;
 
 export default ProjectsList;
